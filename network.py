f60d2d7d2a3beebec214c51e82e8808e84018042
MichaelJaber <mjjaber@ucsd.edu>
2017-09-01 03:15:11 -0700

    Added backpropagation function

--- network.py
+++ network.py
@@ -2,83 +2,99 @@ import system
 
 import numpy as np
 
-# class Network(object):
-#     def __init__(self,layers):
-#         """ Initalizes the weights and biases with a random array
-#          according to a normal distribuation, with a mean of 0,
-#          and a variance of 1. The biases need only be taken from sizes[1:]
-#          since there is no bias present in the first layer of the network. """
-#         self.num_layers = len(layers)
-#         self.sizes = layers
-#         for y in layers[1:]:
-#             self.biases = [np.random.randn(y,1)]          # Creates an array representing all of the biases of each layer, 
-#                                                           # The input layer is not included when creating the bias array,
-#                                                           # since the bias is used to help calculate the output of a layer. 
+class Network(object):
+    def __init__(self,layers):
+        """ Initalizes the weights and biases with a random array
+         according to a normal distribuation, with a mean of 0,
+         and a variance of 1. The biases need only be taken from sizes[1:]
+         since there is no bias present in the first layer of the network. """
+        self.num_layers = len(layers)
+        self.sizes = layers
+        for y in layers[1:]:
+            self.biases = [np.random.randn(y,1)]          # Creates an array representing all of the biases of each layer, 
+                                                          # The input layer is not included when creating the bias array,
+                                                          # since the bias is used to help calculate the output of a layer. 
 
-#         for x , y in zip(layers[:-1], layers[1:]):
-#             self.weights = [np.random.randn(y , x)]          # Creates an array representing all of the weights between the nodes of each respective layer,
-#                                                              # There are no weights protruding out of the output layer.
+        for x , y in zip(layers[:-1], layers[1:]):
+            self.weights = [np.random.randn(y , x)]          # Creates an array representing all of the weights between the nodes of each respective layer,
+                                                             # There are no weights protruding out of the output layer.
 
 
-#     def forward_pass(self,a):
-#         """ Performs a forward pass through the neural network with input,
-#         a, applying the sigmoid function at each node """
-#         for w , b in zip(self.weights, self.bias):
-#             a = sigmoid(np.dot(w , a) + b)
-#         return a
+    def forward_pass(self,a):
+        """ Performs a forward pass through the neural network with input,
+        a, applying the sigmoid function at each node """
+        for w , b in zip(self.weights, self.bias):
+            a = sigmoid(np.dot(w , a) + b)
+        return a
 
-#     def SGD(batches, mini_batch_size, learning_rate, training_data, test_data = None):
-#         """ Stochastic gradient descent takes small batches of the training
-#         data, which will then be passed through the backpropagation algorithm.
-#         New weights and biases will be generated, and the network will be updated
-#         to reflect this. """
+    def SGD(batches, mini_batch_size, learning_rate, training_data, test_data = None):
+        """ Stochastic gradient descent takes small batches of the training
+        data, which will then be passed through the backpropagation algorithm.
+        New weights and biases will be generated, and the network will be updated
+        to reflect this. """
         
-#         ## input cases for test_data
+        ## input cases for test_data
 
-#         for i in range(batches):
-#             random.shuffle(training_data)
-#             for i in range(0, len(training_data), mini_batch_size):
-#                 mini_batches = [training_data[i:i+mini_batch_size]]
-#                 for mini_batch in mini_batches:
-#                     self.update_network(mini_batch, learning rate)
+        for i in range(batches):
+            random.shuffle(training_data)
+            for i in range(0, len(training_data), mini_batch_size):
+                mini_batches = [training_data[i:i+mini_batch_size]]
+                for mini_batch in mini_batches:
+                    self.update_network(mini_batch, learning_rate)
 
-#     def update_network(self, mini_batch, learning rate):
-#         """ Updates the weights and biases of the network according to the new 
-#         values calculated by the backpropogation algorithm. """
-#         for w in self.weights:
-#             grad_wrt_w = [np.zeros(w.shape)]
-#         for b in self.biases:
-#             grad_wrt_b = [np.zeros(b.shape)]
-#         for 
+    def update_network(self, mini_batch, learning_rate):
+        """ Updates the weights and biases of the network according to the new 
+        values calculated by the backpropogation algorithm. """
+        for w in self.weights:
+            grad_wrt_w = [np.zeros(w.shape)]
+        for b in self.biases:
+            grad_wrt_b = [np.zeros(b.shape)]
+        for x , y in mini_batch:
+            delta_grad_wrt_b, delta_grad_wrt_w = self.backpropogation( x , y )
+            for dgrad_b, grad_b in zip(delta_grad_wrt_b, grad_wrt_b):
+                grad_wrt_b = [dgrad_b + grad_b]
+            for dgrad_w, grad_w in zip(delta_grad_wrt_w, grad_wrt_w):
+                grad_wrt_w = [dgrad_w + grad_w]
+        for b , grad_wrt_b in zip(self.biases, grad_wrt_b):
+            self.biases = [ b - ( learning_rate / len(mini_batch)) * grad_wrt_b]
+        for w , grad_wrt_w in zip(self.weights, grad_wrt_w):
+            self.weights = [ w - ( learning_rate / len(mini_batch)) * grad_wrt_w]
 
-#     def backpropogation(self, x, y):
-#         for w in self.weights:
-#             grad_wrt_w = [np.zeros(w.shape)]
-#         for b in self.biases:
-#             grad_wrt_b = [np.zeros(b.shape)]
 
-#         activation = x
-#         activations = [x]                                # activations refers to the weighted inputs of each respective level, after being passed through the sigmoid activation function
-#         zs = []                                          # outputs refers the outputs of each respective level, excluding the sigmoid activation function
-#         for w, b in zip(self.weights, self.biases):
-#             z = np.dot(w, start_activation) + b          # z can be visualized as a weighted input into a layer in the network
-#             z.append(zs)
-#             activation = sigmoid(z)
-#             zs.append(activation)
-
-#         delta = cost_derivative(activation[-1], y) * deriv_sigmoid(zs[-1])
-
-#         grad_wrt_b[-1] = delta
-#         grad_wrt_w[-1] = np.dot(activation[-1], delta)
-
-#         for i in range(2, self.num_layers):
-#             delta = np.dot(self.weights[-l+1].transpose(), delta) * deriv_sigmoid(z[-l])
-#             grad_wrt_b[-l] = delta
-#             grad_wrt_w[-l] = np.dot(activations[-l-1], delta)
-#         return(grad_wrt_b, grad_wrt_w)
+        
 
 
 
+    def backpropogation(self, x, y):
+        """ Takes in a tuple ( x , y ) and returns the gradients with respect to 
+        the biases and weights of the networks. These gradients will be later used
+        to update the network's biases and weights in preparation for the next
+        mini_batch of data. """
+        for w in self.weights:
+            grad_wrt_w = [np.zeros(w.shape)]
+        for b in self.biases:
+            grad_wrt_b = [np.zeros(b.shape)]
+
+        activation = x
+        activations = [x]                                # activations refers to the weighted inputs of each respective level, after being passed through the sigmoid activation function
+        zs = []                                          # outputs refers the outputs of each respective level, excluding the sigmoid activation function
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, activation) + b          # z can be visualized as a weighted input into a layer in the network
+            z.append(zs)
+            activation = sigmoid(z)
+            zs.append(activation)
+        ## The following variables account for when the activation is at an output layer.
+        delta = cost_derivative(activation[-1], y) * deriv_sigmoid(zs[-1])
+
+        grad_wrt_b[-1] = delta
+        grad_wrt_w[-1] = np.dot(activation[-1], delta)
+        ## The following variables account for when the activation is at a hidden layer. """
+        for i in range(2, self.num_layers):
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * deriv_sigmoid(z[-l])
+            
+            grad_wrt_b[-l] = delta
+            grad_wrt_w[-l] = np.dot(activations[-l-1], delta)
+        return(grad_wrt_b, grad_wrt_w)
 
 
 
@@ -87,16 +103,22 @@ import numpy as np
 
 
 
-#     def cost_derivative(activation, y):
 
 
-# ###########################################################################################################################################
 
-# def sigmoid(x):
-#         """ Sigmoid function, used as activation for neurons """
-#         return 1.0 / (1.0 + np.exp(-x))
+    def cost_derivative(output, y):
+        """ Derivative of the cost function, which in this case is
+        is a quadratic loss function. """
+        return output - y
 
-def deriv_sigmoid(x)
+
+###########################################################################################################################################
+
+def sigmoid(x):
+        """ Sigmoid function, used as activation for neurons """
+        return 1.0 / (1.0 + np.exp(-x))
+
+def deriv_sigmoid(x):
         """ Derivative of sigmoid function, used in backpropagation """
         return (sigmoid(x))(1.0 - sigmoid(x))
         
