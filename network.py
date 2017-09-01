1af0818292d960292277ca7ab19c50e6a274320a
Michael Jaber <30811389+MichaelJaber@users.noreply.github.com>
2017-09-01 00:17:25 -0700

    Update network.py

--- network.py
+++ network.py
@@ -11,15 +11,18 @@ class Network(object):
         self.num_layers = len(layers)
         self.sizes = layers
         for y in layers[1:]:
-            self.biases = [np.random.randn(y,1)]
+            self.biases = [np.random.randn(y,1)]          # Creates an array representing all of the biases of each layer, 
+                                                          # The input layer is not included when creating the bias array,
+                                                          # since the bias is used to help calculate the output of a layer. 
 
         for x , y in zip(layers[:-1], layers[1:]):
-            self.weights = [np.random.randn(y , x)]
+            self.weights = [np.random.randn(y , x)]          # Creates an array representing all of the weights between the nodes of each respective layer,
+                                                             # There are no weights protruding out of the output layer.
 
 
     def forward_pass(self,a):
         """ Performs a forward pass through the neural network with input,
-        a, and returns it as an output. """
+        a, applying the sigmoid function at each node """
         for w , b in zip(self.weights, self.bias):
             a = sigmoid(np.dot(w , a) + b)
         return a
@@ -30,11 +33,31 @@ class Network(object):
         New weights and biases will be generated, and the network will be updated
         to reflect this. """
         
+        ## input cases for test_data
+
         for i in range(batches):
+            random.shuffle(training_data)
             for i in range(0, len(training_data), mini_batch_size):
                 mini_batches = [training_data[i:i+mini_batch_size]]
-                for mini_batch in mini_batches
+                for mini_batch in mini_batches:
+                    self.update_network(mini_batch, learning rate)
 
+    def update_network(self, mini_batch, learning rate):
+        """ Updates the weights and biases of the network according to the new 
+        values calculated by the backpropogation algorithm. """
+
+
+    def backpropogation(self, x, y):
+
+
+        activation = x
+        activations = [x]          # activations refers to the inputs of each respective level, including the sigmoid activation function
+        zs = []          # outputs refers the outputs of each respective level, excluding the sigmoid activation function
+        for w, b in zip(self.weights, self.biases):
+            z = np.dot(w, start_activation) + b          # z represents the output of a layer before it is passed through the activation function
+            z.append(zs)
+            activation = sigmoid(z)
+            zs.append(activation)
 
 
 
